64b6d1b3a89eb0fca011caaaa62d5f0f3be35221
vaibhav bendre <vbendre87@gmail.com>
2018-10-22 23:56:06 +0530

    Exposing service to process and check word

--- src/main/java/hello/WordController.java
+++ src/main/java/hello/WordController.java
@@ -1,8 +1,25 @@
 package hello;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import hello.service.WordService;
+import hello.service.model.Word;
 
 @RestController
 public class WordController {
-    // TODO Implement the /words/{word} endpoint
+    
+	@Autowired 
+	private WordService service;
+	
+	@GetMapping("/words/{word}")
+	public Word processWord(@PathVariable String word) {
+		final Word processedWord = new Word();
+		processedWord.setWord(word);
+		processedWord.setAnagramOfPalindrome(service.isAnagramPalindrome(word));
+		processedWord.setPalindrome(service.isPalindrome(word));
+		return processedWord;
+	}
 }
